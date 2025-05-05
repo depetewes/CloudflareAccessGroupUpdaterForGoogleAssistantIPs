767a507ef5f549c77a165bb63277737770ccb79b
depetewes <77380830+depetewes@users.noreply.github.com>
2025-05-05 17:23:22 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,26 +1,27 @@
-import cloudflare
+from cloudflare import Cloudflare
 import requests
 
 def get_google_addresses():
     r = requests.get("https://www.gstatic.com/ipranges/goog.json")
     r.raise_for_status()
-    ips = [ entry['ipv4Prefix'] for entry in r.json()['prefixes'] if 'ipv4Prefix' in entry ]
-    ips += [ entry['ipv6Prefix'] for entry in r.json()['prefixes'] if 'ipv6Prefix' in entry ]
+    ips = [entry['ipv4Prefix'] for entry in r.json()['prefixes'] if 'ipv4Prefix' in entry]
+    ips += [entry['ipv6Prefix'] for entry in r.json()['prefixes'] if 'ipv6Prefix' in entry]
     return ips
 
 def update_access_group(token, account_id, group_id, ips):
-    cf = cloudflare.Cloudflare(api_token=args.token)  # Korrekt brug af CloudFlare SDK
-    
+    cf = Cloudflare(token=token)
+
     data = {
-        "include": [ {"ip": {"ip": ip}} for ip in ips ],
+        "include": [{"ip": {"ip": ip}} for ip in ips],
         "exclude": [],
         "require": []
     }
-    cf.accounts.access.groups.put(account_id, group_id, data=data)
+
+    cf.zero_trust.access.groups.update(account_id, group_id, data=data)
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="CloudFlare Google Assistant Access Group updater")
+    parser = argparse.ArgumentParser(description="Cloudflare Google Assistant Access Group updater")
     parser.add_argument("--account", required=True, help="CF account ID")
     parser.add_argument("--token", required=True, help="CF API key")
     parser.add_argument("--group", required=True, help="ID of CF Access group that needs to be updated")
